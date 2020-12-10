6f39116a396a88cbc447b86b20d0b34b0ffdfed8
hyaz <hyaz@hy.com>
2020-12-10 14:18:01 +0800

    init

---
+++ CStest/Assets/Script/testcshader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testcshader : MonoBehaviour
+{
+    public float a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a=Time.deltaTime;
+    }
+}
